5c9b0f8146631b6c58118ccfad7c6df69537a0f0
Krylov Alexey <alexey2142@mail.ru>
2015-12-16 11:09:56 +1000

    Фикс ошибки с title == nil при выводе альбомов

--- app/helpers/active_admin/view_helpers/dropzone_helper.rb
+++ app/helpers/active_admin/view_helpers/dropzone_helper.rb
@@ -1,10 +1,19 @@
 module ActiveAdmin::ViewHelpers::DropzoneHelper
 
+  def dropzone_object_title_by_index dropzone_object, index
+    title = dropzone_object.send(dropzone_object.class.dropzone_field(:title))
+    if title.present?
+      title.squish
+    else
+      "#{I18n.t('dropzone.labels.image')} \##{dropzone_object.send('id')}"
+    end
+  end
+
   def render_mock_dropzone_files(dropzone_objects)
     dropzone_objects.map.with_index do |dropzone_object, index|
       {
         id: dropzone_object.id,
-        name: dropzone_object.send(dropzone_object.class.dropzone_field(:title)).squish,
+        name: dropzone_object_title_by_index(dropzone_object, index),
         size: dropzone_object.send(dropzone_object.class.dropzone_field(:file_size)),
         url: dropzone_object.send(dropzone_object.class.dropzone_field(:url)),
         index: index,
